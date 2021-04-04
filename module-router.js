2e32000d2992d79d55a2de525a705944e9d48c7d
cryptosenal <81895945+cryptosenal@users.noreply.github.com>
2021-04-04 12:19:20 -0500

    Update module-router.js

--- module-router.js
+++ module-router.js
@@ -3,19 +3,19 @@ var routeo = function(req, res){
 
    switch(req.url){
     case '/':
-     res.send("Index Inicio"); //aqui lo que quieras que haga al llegar a /home
+     res.send("Index Inicio"); //Aqui va los archivos de Inicio Promedio de la App
    break;
     case '/1inchusdt/:time':
-      res.send("Nada 2"); //aqui lo que quieras que haga al llegar a /contact
+      res.send("Nada 2"); //Aqui va 1inchusdt generalizada y dividida x :time 5m/15m/30m/1h/2h/4h
    break;
     case '/aaveusdt/:time':
-      res.send("Nada 2"); //aqui lo que quieras que haga al llegar a /contact
+      res.send("Nada 2"); //Aqui va 1inchusdt generalizada y dividida x :time 5m/15m/30m/1h/2h/4h
    break;
     case '/adausdt/:time':
-      res.send("Nada 2"); //aqui lo que quieras que haga al llegar a /contact
+      res.send("Nada 2"); //Aqui va 1inchusdt generalizada y dividida x :time 5m/15m/30m/1h/2h/4h
    break;
    default:
-      res.send("Nada 3"); //aqui entrará todo lo que no cumpla con los cases ejemplo un 404
+      res.send("Error 404"); //aqui entrará todo lo que no cumpla con los cases ejemplo un 404
    
    }
   
