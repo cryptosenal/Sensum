e46164eec12501a9e32b00b4ac9ddd88580dcecc
cryptosenal <81895945+cryptosenal@users.noreply.github.com>
2021-04-04 12:21:05 -0500

    Update module-router.js

--- module-router.js
+++ module-router.js
@@ -2,17 +2,17 @@
 var routeo = function(req, res){
 
    switch(req.url){
-    case '/':
-     res.send("Index Inicio"); //Aqui va los archivos de Inicio Promedio de la App
+    case '/':  //Aqui va los archivos de Inicio Promedio de la App
+     res.send("Index Inicio");
    break;
-    case '/1inchusdt/:time':
-      res.send("Nada 2"); //Aqui va 1inchusdt generalizada y dividida x :time 5m/15m/30m/1h/2h/4h
+    case '/1inchusdt/:time':  //Aqui va 1inchusdt generalizada y dividida x :time 5m/15m/30m/1h/2h/4h
+      res.send("Nada 2");
    break;
-    case '/aaveusdt/:time':
-      res.send("Nada 2"); //Aqui va 1inchusdt generalizada y dividida x :time 5m/15m/30m/1h/2h/4h
+    case '/aaveusdt/:time': //Aqui va 1inchusdt generalizada y dividida x :time 5m/15m/30m/1h/2h/4h
+      res.send("Nada 2"); 
    break;
-    case '/adausdt/:time':
-      res.send("Nada 2"); //Aqui va 1inchusdt generalizada y dividida x :time 5m/15m/30m/1h/2h/4h
+    case '/adausdt/:time': //Aqui va 1inchusdt generalizada y dividida x :time 5m/15m/30m/1h/2h/4h
+      res.send("Nada 2"); 
    break;
    default:
       res.send("Error 404"); //aqui entrará todo lo que no cumpla con los cases ejemplo un 404
